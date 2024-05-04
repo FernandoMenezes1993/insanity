e3b484cffc5988b16ea036399fc919c7b57fc5a1
Fernando <fernando.menezes@conversion.com.br>
2024-05-03 22:25:52 -0300

    feat: Finalizando tela de cadastro

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -11,6 +11,7 @@ const BackURL = import.meta.env.VITE_URL;
 
 const Cadastro = () =>{
     const navigate = useNavigate();
+    const [nameVerified, setNameVerified] = useState(1);
 
     /*Formatação dos inputs*/
     const [inputNickname, setInputNickname] = useState('CadastroInput')
@@ -37,11 +38,13 @@ const Cadastro = () =>{
     const [titleModal, setTitleModal] = useState("");
     const [msgModal, setMsgModal] = useState("");
 
+    const[cssModalBody, setCssModalBody] = useState("");
+    const[imgModal, setImgModal] = useState("")
+
     /*Chamando api com nome dos membros da guilda*/
     const [membrosGuilda, setMembrosGuilda] = useState();
 
     const getMembrosGuilda = async()=>{
-        console.log("aqui")
         try {
             const res = await fetch(`${BackURL}/api/playresGuild`);
             if(!res.ok){
@@ -70,12 +73,87 @@ const Cadastro = () =>{
                     if(senha == confirmarSenha){                        
                         setInputConfirmarSenha ("CadastroInput");
                         setInputSenha("CadastroInput");
-
-                        /* AQUI IREMOS FAZER O CADASTRO DO CLIENTE */
-
-                        console.log(membrosGuilda);
                         
+                        try {
+                            const checksNameRegistered  = await fetch(`${BackURL}/api/checks/name/${nickname}`);
+                            if (!checksNameRegistered.ok) {
+                                throw new Error(`HTTP error! status: ${response.status}`);
+                            };
+                            const res = await checksNameRegistered.json();
+                            const resApi = res.length
+                            
+                            if(resApi == 1){
+
+                                let membroEncontrado = false;
+                                const qntMember = membrosGuilda.length;
+                                for (let i = 0; i < qntMember; i++){
+                                    if(nickname == membrosGuilda[i].nome){
+                                        const newUser ={
+                                            Name: nickname,
+                                            Senha: senha,
+                                            Email: email,
+                                            Discordid: discordId
+                                        };
+                                        try {
+                                            const response = await fetch(`${BackURL}/api/user/new`, {
+                                            method: 'POST',
+                                            headers: {
+                                                'Content-Type': 'application/json'
+                                            },
+                                                body: JSON.stringify(newUser)
+                                            });
+                                    
+                                            if (!response.ok) {
+                                                throw new Error(`HTTP error! status: ${response.status}`);
+                                            }
+                                    
+                                            const result = await response.json();
+
+                                            // Alerta de cadsatro realizado
+                                            setTitleModal("Cadastrado");                                            
+                                            setMsgModal("Cadastro realizado!");
+                                            setImgModal('./img/msg/imgModalCompleto.webp');
+                                            setCssModalBody("ModalBodyCadastrado");
+                                            handleOpen()
+
+                                            setTimeout(voltarHome, 3000);
+
+                                        } catch (error) {
+                                            console.error('Error:', error);
+                                            alert('Erro ao criar usuário');
+                                        }
+                                        membroEncontrado = true;
+                                        break;
+                                    }
+                                }
+                                if (!membroEncontrado) {
+                                    setTitleModal("Erro");                                            
+                                    setMsgModal("Você não está na guilda!");
+                                    setImgModal('./img/msg/erro.png');
+                                    setCssModalBody("ModalBodyError");
+                                    handleOpen()
+
+                                    setNickname("")
+                                    setSenha("")
+                                    setConfirmarSenha("")  
+                                    setEmail("")
+                                    setDiscordId("")
+                                }
+                            }else{
+                                setTitleModal("Erro");                                            
+                                setMsgModal("Você já está cadastrado!");
+                                setImgModal('./img/msg/erro.png');
+                                setCssModalBody("ModalBodyError");
+                                handleOpen()
+
+                                setTimeout(voltarHome, 3000);
+                            }
+                        } catch (error) {
+                            console.error(error)
+                        }   
                     }else{
+                        setCssModalBody("ModalBodyError")
+                        setImgModal('./img/msg/erro.png')
                         setInputSenha("SenhaInputErro")
                         setInputConfirmarSenha('ConfirmarSenhaInputErro')
                         setTitleModal("ERRO")
@@ -85,18 +163,24 @@ const Cadastro = () =>{
                         handleOpen()
                     }
                 }else{
+                    setCssModalBody("ModalBodyError")
+                    setImgModal('./img/msg/erro.png')
                     setInputConfirmarSenha('ConfirmarSenhaInputErro')
                     setTitleModal("ERRO")
                     setMsgModal("Digite a confirmação!")            
                     handleOpen()
                 }
             }else{
+                setCssModalBody("ModalBodyError")
+                setImgModal('./img/msg/erro.png')
                 setInputSenha("SenhaInputErro")
                 setTitleModal("ERRO")
                 setMsgModal("Digite uma senha!")            
                 handleOpen()
             }            
         }else{
+            setCssModalBody("ModalBodyError")
+            setImgModal('./img/msg/erro.png')
             setInputNickname("NicknameInputErro")
             setTitleModal("ERRO")
             setMsgModal("Digite seu Nickname!")            
@@ -130,16 +214,14 @@ const Cadastro = () =>{
                     <button className='ButtonCadastro' onClick={registerUser}>Cadastrar</button>
                     <button className='ButtonVoltar' onClick={voltarHome}>Voltar</button>
                 </div>
-            </div>
-
-            
+            </div>           
 
             <Modal open={open} onClose={handleClose} className='ModalError' size="300px">
                 <Modal.Header>
                     <Modal.Title className='ModalTitle'>{titleModal}</Modal.Title>
                 </Modal.Header>
-                <Modal.Body className='ModalBody'>
-                    <img src="./img/msg/erro.png" alt="Error" className='ModalBodyImg' />
+                <Modal.Body className={cssModalBody}>
+                    <img src={imgModal} alt="Error" className='ModalBodyImg' />
                     <p className='ModalBodyP'>{msgModal}</p>
                 </Modal.Body>            
             </Modal>
